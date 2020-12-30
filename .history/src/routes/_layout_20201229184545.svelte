3fc617fa2f49a572b084e8fc66cdb306354628f8
Joe Mooney <joe.mooney@gmail.com>
2020-12-29 18:48:57 -0700

    first commit

---
+++ .history/src/routes/_layout_20201229184545.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nav from "../components/Nav.svelte";
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<h1>sss</h1>
+<Nav {segment} />
+
+<main>
+	<slot />
+</main>
